fd2a947ebd5242b9b86e8ea962bc1275ad7c37e5
Colin Shevlin <cwshevlin@gmail.com>
2015-01-24 13:20:23 -0600

    Added teacher mailers.

---
+++ app/mailers/teacher_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "no-reply@stuccostuc.co"
+
+  def welcome_email(teacher)
+  	@teacher = teacher
+  	mail(to: @teacher.email, subject: 'Welcome to Stucco')
+  end
+  
+end
