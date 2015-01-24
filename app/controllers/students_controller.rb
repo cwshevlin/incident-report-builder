7e206921905813c7e974eb658084a9b90163c0e2
Colin Shevlin <cwshevlin@gmail.com>
2015-01-24 12:45:06 -0600

    Fleshed out student controllers.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,27 +1,32 @@
 class StudentsController < ApplicationController
 
 	def index
+		@students = Student.all
 	end
 
 	def show
+		@student = student.find_by(id: params[:id])
 	end
 
 	def new
-		
+		@student = Student.new
 	end
 
 	def create
-
+		@student = Student.new(params[:student])
+		redirect_to students_path
 	end
 
 	def edit
-
+		@student = student.find_by(id: params[:id])
 	end
 
 	def update
+		
 	end
 
 	def delete
+
 	end
 
 end
