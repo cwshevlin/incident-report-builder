45f78eb3fd488c0b0abbd8f5695893e13e182c74
Colin Shevlin <cwshevlin@gmail.com>
2015-01-23 23:05:09 -0600

    Added a CSV parsing method in the student model.

--- incident-reports/app/models/student.rb
+++ incident-reports/app/models/student.rb
@@ -1,6 +1,15 @@
+require 'csv'
+
 class Student < ActiveRecord::Base
 	belongs_to :teacher
 	has_many :student_incidents
 	has_many :incidents, through: :student_incidents
+
+	def csv_to_students(csv_file_path)
+		CSV.foreach("#{csv_file_path}") do |row|
+	  	student = Student.new(first_name: row[1], last_name: row.first, grade: row.last)
+	  	student.save unless student.grade.nil?
+		end
+	end
 
 end
