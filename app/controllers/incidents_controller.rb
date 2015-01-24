d274cdd43885a833ae5b0bd85308c8deb922f59f
Colin Shevlin <cwshevlin@gmail.com>
2015-01-24 12:14:14 -0600

    Fleshed out the incidents controller.

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,15 +1,15 @@
 class IncidentsController < ApplicationController
 
 	def index
-		
+		@incidents = Incident.all
 	end
 
 	def show
-		
+		@incident = Incident.find_by(id: params[:id])
 	end
 
 	def new
-		
+		@incident = Incident.new
 	end
 
 	def create
@@ -17,13 +17,16 @@ class IncidentsController < ApplicationController
 	end
 
 	def edit
-
+		@incident = Incident.find_by(id: params[:id])
 	end
 
 	def update
+		@incident = Incident.find_by(id: params[:id])
+		
 	end
 
 	def delete
+
 	end
 
 
