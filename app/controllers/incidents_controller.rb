dc513127294c5f8244de70b061f6ef5b5daf9c64
Colin Shevlin <cwshevlin@gmail.com>
2015-01-24 16:46:21 -0600

    Fleshing out the controllers.

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -38,7 +38,7 @@ class IncidentsController < ApplicationController
 	private
 
 		def incident_params
-			parms = [:date_time, :expectation_violated]#, :major_or_minor, :anecdote, :location, :context, :duration, :possible_function, :staff_responses, :effect_on_behavior]
+			params = [:date_time, :expectation_violated]#, :major_or_minor, :anecdote, :location, :context, :duration, :possible_function, :staff_responses, :effect_on_behavior]
 			params.require(:date_time).permit(:expectation_violated) # change to user
 		end
 end
